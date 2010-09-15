5e59bf02594afd90949a32c2f375752ffc705fad
Sridhar Ratnakumar <me@srid.name>
2010-09-15 16:07:48 -0700

    Add a setup.py and tox.ini for use in hudson

---
+++ setup.py
@@ -0,0 +1,9 @@
+# A dummy setup script so we can run toxbootstrap.py on this project itself.
+# http://hudson.activestate.com/hudson/job/tox-bootstrap/
+
+from setuptools import setup
+
+setup(name='dummy',
+      version=0.1,
+      description="Dummy module",
+      )
